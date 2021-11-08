9819f5f988e994183696bb819f20c49573f56b36
https://github.com/Leticiafrnc <leticiafrnc1@gmail.com>
2021-11-08 20:10:26 -0300

    finalizado

---
+++ README.md
@@ -0,0 +1,26 @@
+
+# Atividade Letra Inicial
+### Esse projeto foi desenvolvido para auxilar crianças do 1º e 2º anos do Ensino Fundamental I . Juntei a informática com a educação para assim ajudar as crianças na alfabetização e treinar minhas habilidades com front-end.
+
+<h4 align="center"> 
+🚀 Finalizado
+</h4>
+
+### 🏁 Instalação
+
+Antes de começar, é bom ter um editor para trabalhar com o código como [VSCode](https://code.visualstudio.com/)
+Clone este repositório usando git clone: https://github.com/Leticiafrnc/Atividade-letra-inicial.git ou clique em Code/Download ZIP. Clique no arquivo "index.HTML" e veja a aplicação.
+
+### 🛠 Tecnologias
+
+As seguintes ferramentas foram usadas na construção do projeto:
+- HTML
+- Css
+- Bootstrap v5.0
+
+
+### ✏ Autora
+
+Feito com ❤️ por Letícia Franco 👋🏽 Entre em contato!
+
+ [<img src="https://img.shields.io/badge/linkedin-%230077B5.svg?&style=for-the-badge&logo=linkedin&logoColor=white" />](https://www.linkedin.com/in/leticiafrnc//) 
---
+++ app.js
@@ -0,0 +1,117 @@
+function conferirRato() {
+  var respA = document.getElementById("respA").value;
+  const respO = document.getElementById("respO").value
+
+  if (respA === "a" && respO === "o") // verifa se os dois são verdadeiros
+  {
+    document.getElementById("respRato").innerHTML = "Parabéns! Você acertou";
+  }
+  else {
+    document.getElementById("respRato").innerHTML = "Resposta Errada, tente novamente!";
+  }
+}
+
+function conferirBala() {
+  var respA = document.getElementById("resptA").value;
+  const resptA = document.getElementById("resptA").value
+
+  if (respA === "a" && resptA === "a") // verifa se os dois são verdadeiros
+  {
+    document.getElementById("respBala").innerHTML = "Parabéns! Você acertou";
+  }
+  else {
+    document.getElementById("respBala").innerHTML = "Resposta Errada, tente novamente!";
+  }
+}
+
+function conferirMola() {
+  var respostaMo = document.getElementById("respostaMo").value;
+  const respostaMa = document.getElementById("respostaMa").value
+
+  if (respostaMo === "o" && respostaMa === "a") // verifa se os dois são verdadeiros
+  {
+    document.getElementById("respMola").innerHTML = "Parabéns! Você acertou";
+  }
+  else {
+    document.getElementById("respMola").innerHTML = "Resposta Errada, tente novamente!";
+  }
+}
+
+function conferirBolo() {
+  var respostaBo = document.getElementById("respostaBo").value;
+  const respostaLo = document.getElementById("respostaLo").value
+
+  if (respostaBo === "o" && respostaLo === "o") // verifa se os dois são verdadeiros
+  {
+    document.getElementById("respBolo").innerHTML = "Parabéns! Você acertou";
+  }
+  else {
+    document.getElementById("respBolo").innerHTML = "Resposta Errada, tente novamente!";
+  }
+}
+
+
+function conferirBule() {
+  var respostaBu = document.getElementById("respostaBu").value;
+  const respostaLe = document.getElementById("respostaLe").value
+
+  if (respostaBu === "u" && respostaLe === "e") // verifa se os dois são verdadeiros
+  {
+    document.getElementById("respBule").innerHTML = "Parabéns! Você acertou";
+  }
+  else {
+    document.getElementById("respBule").innerHTML = "Resposta Errada, tente novamente!";
+  }
+}
+
+function conferirFogo() {
+  var respostaFo = document.getElementById("respostaFo").value;
+  const respostaGo = document.getElementById("respostaGo").value
+
+  if (respostaFo === "o" && respostaGo === "o") // verifa se os dois são verdadeiros
+  {
+    document.getElementById("respFogo").innerHTML = "Parabéns! Você acertou";
+  }
+  else {
+    document.getElementById("respFogo").innerHTML = "Resposta Errada, tente novamente!";
+  }
+}
+
+function conferirMala() {
+  var respostaMAl = document.getElementById("respostaMAl").value;
+  const respostaLal = document.getElementById("respostaLal").value
+
+  if (respostaMAl === "a" && respostaLal === "a") // verifa se os dois são verdadeiros
+  {
+    document.getElementById("respMala").innerHTML = "Parabéns! Você acertou";
+  }
+  else {
+    document.getElementById("respMala").innerHTML = "Resposta Errada, tente novamente!";
+  }
+}
+
+function conferirRede() {
+  var respostaRE = document.getElementById("respostaRE").value;
+  const respostaDe = document.getElementById("respostaDe").value
+
+  if (respostaRE === "e" && respostaDe === "e") // verifa se os dois são verdadeiros
+  {
+    document.getElementById("respRede").innerHTML = "Parabéns! Você acertou";
+  }
+  else {
+    document.getElementById("respRede").innerHTML = "Resposta Errada, tente novamente!";
+  }
+}
+
+function conferirPato() {
+  var respostaPa = document.getElementById("respostaPa").value;
+  const respostaTo = document.getElementById("respostaTo").value
+
+  if (respostaPa === "a" && respostaTo === "o") // verifa se os dois são verdadeiros
+  {
+    document.getElementById("respPato").innerHTML = "Parabéns! Você acertou";
+  }
+  else {
+    document.getElementById("respPato").innerHTML = "Resposta Errada, tente novamente!";
+  }
+}
